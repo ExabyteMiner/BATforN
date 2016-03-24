3e8e203852131df7dc71daaaec2fceff0ec67b77
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 08:53:31 -0400

    Added files via upload

---
+++ Scripts/VerifyMessage.bat
@@ -0,0 +1,20 @@
+@echo off
+color 0a
+SET /P addy=What is the address on which the message was signed?
+echo %addy%> ..\core\addy.txt
+SET /P sig=What is the signature hash?
+echo %sig%> ..\core\sig.txt
+SET /P msg=What is the EXACT message as it was signed?
+echo %msg%> ..\core\msg.txt
+SET /P data=<..\core\data.txt
+SET /P conf=<..\core\conf.txt
+SET /P add=<..\core\addy.txt
+SET /P str=<..\core\sig.txt
+SET /P mse=<..\core\msg.txt
+SET bitd=coind -datadir=%data% -conf=%conf%
+SET ver=%bitd% verifymessage "%add%" "%str%" "%mse%"
+%ver%
+del ..\core\addy.txt
+del ..\core\sig.txt
+del ..\core\msg.txt
+pause
