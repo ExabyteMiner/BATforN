db89ab42ffac94ec7d4edcb96f6a29194709fd83
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:16:51 -0400

    Update GetBalance.bat

--- Scripts/GetBalance.bat
+++ Scripts/GetBalance.bat
@@ -2,7 +2,5 @@
 color 0a
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET bal=%bitd% getbalance
-%bal%
+coind -datadir=%data% -conf=%conf% getbalance
 pause
