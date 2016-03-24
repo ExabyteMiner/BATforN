d3fefd3312a222a79d04f42332caf7417a23e811
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:29:10 -0400

    Update GetStakingInfo.bat

--- Scripts/GetStakingInfo.bat
+++ Scripts/GetStakingInfo.bat
@@ -2,7 +2,5 @@
 color 0a
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET stk=%bitd% getstakinginfo
-%stk%
+coind -datadir=%data% -conf=%conf% getstakinginfo
 pause
