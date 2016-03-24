ea568dfce4e37f6297b8ed73cc77dfe7c6f0142e
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:34:23 -0400

    Update ListAccounts.bat

--- Scripts/ListAccounts.bat
+++ Scripts/ListAccounts.bat
@@ -2,7 +2,5 @@
 color 0a
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET lst=%bitd% listaccounts
-%lst%
+coind -datadir=%data% -conf=%conf% listaccounts
 pause
