10fefe3813f8c8d22375bf118acabc268df75208
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:09:38 -0400

    Update EncryptWallet.bat

--- Scripts/EncryptWallet.bat
+++ Scripts/EncryptWallet.bat
@@ -1,13 +1,11 @@
 @echo off
 color 0a
 SET /P pass=Please enter the new password (I recommend a random series of letters and numbers with upper AND lower case):
-echo %pass%> ..\core\pass.txt
+ECHO %pass%> ..\core\pass.txt
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
 SET /P key=<..\core\pass.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET crp=%bitd% encryptwallet %key%
-%crp%
-del ..\core\pass.txt
-echo The password has been set to "%key%".
+coind -datadir=%data% -conf=%conf% encryptwallet %key%
+DEL ..\core\pass.txt
+ECHO Success! The password has been set to "%key%".
 pause
