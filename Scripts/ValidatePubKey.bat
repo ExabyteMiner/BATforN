86b597455a4cdd1e12c0e3d5f6a1fa78dc06b3f4
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:49:12 -0400

    Update ValidatePubKey.bat

--- Scripts/ValidatePubKey.bat
+++ Scripts/ValidatePubKey.bat
@@ -1,12 +1,10 @@
 @echo off
 color 0a
 SET /P addy=What is the pubkey you would like to validate?
-echo %addy%> ..\core\addy.txt
+ECHO %addy%> ..\core\addy.txt
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
 SET /P add=<..\core\addy.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET val=%bitd% validatepubkey %add%
-%val%
-del ..\core\addy.txt
+coind -datadir=%data% -conf=%conf% validatepubkey %add%
+DEL ..\core\addy.txt
 pause
