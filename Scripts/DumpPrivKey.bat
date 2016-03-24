6bf3d9b6adc7b483e22d7158d50be99c52ace15f
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:10:52 -0400

    Update DumpPrivKey.bat

--- Scripts/DumpPrivKey.bat
+++ Scripts/DumpPrivKey.bat
@@ -5,9 +5,8 @@ ECHO %addy%> ..\core\addy.txt
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
 SET /P add=<..\core\addy.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
 MKDIR ..\OUTPUTS
-%bitd% dumpprivkey %add%> ..\OUTPUTS\privkey.txt
+coind -datadir=%data% -conf=%conf% dumpprivkey %add%> ..\OUTPUTS\privkey.txt
 DEL ..\core\addy.txt
 ECHO Your privkey is in the "BATforN\OUTPUTS" folder.
 pause
