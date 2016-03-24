eb9a66009adac6ea4c113899b65621c8584273f3
ExabyteMiner <exabyte.miner@aol.com>
2016-03-24 12:46:57 -0400

    Update SignMessage.bat

--- Scripts/SignMessage.bat
+++ Scripts/SignMessage.bat
@@ -1,18 +1,16 @@
 @echo off
 color 0a
-MKDIR ..\OUTPUTS
 SET /P addy=What is the address you would like to sign?
-echo %addy%> ..\core\addy.txt
+ECHO %addy%> ..\core\addy.txt
 SET /P msg=What message do you wish to sign with (NO SPACES!)?
-echo %msg%> ..\core\msg.txt
+ECHO %msg%> ..\core\msg.txt
 SET /P data=<..\core\data.txt
 SET /P conf=<..\core\conf.txt
 SET /P add=<..\core\addy.txt
 SET /P txt=<..\core\msg.txt
-SET bitd=coind -datadir=%data% -conf=%conf%
-SET msg=%bitd% signmessage "%add%" "%txt%"
-%msg%> ..\OUTPUTS\SignedMessage.txt
-::del ..\core\addy.txt::
-::del ..\core\msg.txt::
-echo Your message is in "BATforN\OUTPUTS" folder
+MKDIR ..\OUTPUTS
+coind -datadir=%data% -conf=%conf% signmessage "%add%" "%txt%"> ..\OUTPUTS\SignedMessage.txt
+DEL ..\core\addy.txt
+DEL ..\core\msg.txt
+ECHO Success! Your message hash is in "BATforN\OUTPUTS" folder
 pause
